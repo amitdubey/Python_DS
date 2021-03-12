bc2ef0305ee53c4a9e506ad3b3b41e9ad9deb7e2
Amit Dubey <amit.dubey@temple.edu>
2021-03-11 22:47:28 -0800

    g

--- list_comprehension_basics.py
+++ list_comprehension_basics.py
@@ -11,3 +11,16 @@ print([i*i for i in lst])
 #increment of list by n number
 
 print([(i+1) for i in lst])
+
+#matrix transpose using list comprehension
+matrix = [ [1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 10, 11, 12]]
+
+print(matrix,"is transposed to ",[[row[i] for row in matrix] for i in range(4)])
+# transpose using zip
+
+print(list(zip(*matrix)))
+
+#set comprehensions
+print({x for x in 'abracadabra' if x not in 'abc'})
